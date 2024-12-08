34aa55a1c9c8ee5a0d4294f9405b28eec123c226
EK547 <17ganapathy@gmail.com>
2024-12-08 13:58:40 +0000

    initial server file

---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+from _thread import *
+import sys
+
+server = "192.168.0.35"
+port = 5555
+
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("waiting for connection")
+
+
+
+
+def threaded_client(connection):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+while True:
+    conn,addr = s.accept()
+    print("Connected to:", addr)
+    start_new_thread(threaded_client, (conn,))
